23a0bd22678a60401818a448ff4b45d52b4518b7
mandilorenzo <amanda.lorenzo@hotmail.com>
2025-05-10 14:22:01 -0300

    implementando a função getPosts no componente

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { PostService, Post } from '../../services/post.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-posts',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.css'
 })
-export class PostsComponent {
 
+export class PostsComponent {
+  posts: Post[] = [];
+
+  constructor(private postService: PostService) {}
+
+  ngOnInit() {
+    this.postService.getPosts().subscribe(data => {
+      this.posts = data;
+    });
+  }
 }
+
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-type Post = {
+export interface Post {
   id: number;
   title: string;
   body: string;
