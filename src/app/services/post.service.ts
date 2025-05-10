0f98a8526889d76e068871597959182c13f318b3
mandilorenzo <amanda.lorenzo@hotmail.com>
2025-05-10 12:57:45 -0300

    criação do service/post e components/posts

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+type Post = {
+  id: number;
+  title: string;
+  body: string;
+  createdAt: string;
+  image: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PostService {
+  private apiUrl = 'https://5cf9ae9df26e8c00146cff8d.mockapi.io/api/v1/post';
+
+  constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.apiUrl);
+  }
+}
